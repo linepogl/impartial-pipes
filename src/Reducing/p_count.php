d0a4a8c1a918e37b2c1600ae28b531670b235a01
Lazare Inepologlou <linepogl@gmail.com>
2025-08-08 19:18:23 +0200

    docs

--- src/Reducing/p_count.php
+++ src/Reducing/p_count.php
@@ -10,6 +10,38 @@ use Countable;
  * Partial function to count the element of the iterable that satisfy some optional predicate. If no predicate is
  * provided, the function counts the number of all the elements in the iterable.
  *
+ * ### Syntax
+ * ```
+ * p_count(
+ *   [callable(TValue[, TKey]): bool]
+ * )
+ * ```
+ *
+ * ### Examples
+ * Count without a predicate
+ * ```
+ * []
+ * |> p_count()
+ * //= 0
+ * ```
+ * ```
+ * [1, 2]
+ * |> p_count()
+ * //= 2
+ * ```
+ * Count with a value predicate
+ * ```
+ * [1, 2, 3, 4, 5]
+ * |> p_count(fn (int $x) => $x % 2 === 1)
+ * //= 3
+ * ```
+ * Count with a value and key predicate
+ * ```
+ * ['a' => 1, 'bb' => 2, 'c' => 3, 'd' => 3]
+ * |> p_any(fn (int $x, string $key) => strlen($key) === 1)
+ * //= 3
+ * ```
+ *
  * @template K
  * @template V
  * @param ?callable(V,K):bool $predicate
