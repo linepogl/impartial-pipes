53524fa6c32f5bf5e7e09d17a0a5c43d3b516405
Lazare Inepologlou <linepogl@gmail.com>
2025-08-09 15:02:53 +0200

    coverage

--- dev/coverage.php
+++ dev/coverage.php
@@ -6,4 +6,6 @@ $attr = simplexml_load_file($argv[1])->attributes();
 $c = intval($attr['lines-covered']);
 $t = intval($attr['lines-valid']);
 echo 'Code coverage: ' . $c . '/' . $t . ' (' . round($c / $t * 100, 5) . '%)' . PHP_EOL;
-if ($c !== $t) exit(1);
+if ($c !== $t) {
+    exit(1);
+}
