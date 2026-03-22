19329908425cb2fc4274e7dc14ebd17545a494f5
Lazare Inepologlou <linepogl@gmail.com>
2026-03-22 14:10:14 +0100

    assoc variants

--- src/Mapping/p_then_by.php
+++ src/Mapping/p_then_by.php
@@ -145,8 +145,8 @@ namespace ImpartialPipes;
  */
 function p_then_by(callable $hasher, bool $descending = false): callable
 {
-    // @phpstan-ignore return.type (Closure is a callable)
-    return static fn (AssociativeOrderedIterator|ListOrderedIterator $iterable) =>
-        // @phpstan-ignore argument.type ($iterable is in fact a XxxOrderedIterator<K,V>) */
-        $iterable->thenBy($hasher, $descending);
+    return static function (AssociativeOrderedIterator|ListOrderedIterator $iterable) use ($hasher, $descending) {
+        /** @var AssociativeOrderedIterator<K,V>|ListOrderedIterator<K,V> $iterable */
+        return $iterable->thenBy($hasher, $descending);
+    };
 }
